9c403584d634f2e89751fe99794e673314f9eaa7
Raymond Giang <raymondgiang@gmail.com>
2020-04-27 15:24:44 -0400

    initial commit of template

---
+++ src/routes/about.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<style lang="scss">
+	@import '../../style/theme.scss';
+	
+	h1 {
+		@include test;
+
+		span {
+			color: green;
+		}
+	}
+</style>
+
+<h1><span>About this site</span></h1>
+
+<p>This is the 'about' page. There's not much here.</p>
